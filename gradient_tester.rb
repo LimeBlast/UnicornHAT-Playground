8ebe7569e082db1a27121d8700ff30894e90f551
Daniel Hollands <daniel@limeblast.co.uk>
2016-04-03 15:29:37 +0000

    Added gradient class and tester

--- gradient_tester.rb
+++ gradient_tester.rb
@@ -3,23 +3,24 @@ require 'ws2812'
 require 'color'
 require 'color-generator'
 
-hat          = Ws2812::UnicornHAT.new
-hat.rotation = 90
+require_relative 'gradient'
 
-generator = ColorGenerator.new(saturation: 0.75, lightness: 0.4)
+cg  = ColorGenerator.new(saturation: 0.75, lightness: 0.4)
+hat = Ws2812::UnicornHAT.new
 
 begin
   puts 'Hit ^C to terminate...'
+  hat.rotation = 90
   hat.clear false
   loop do
+    g = Gradient.new(cg.create_rgb, cg.create_rgb, 7)
     (0..7).each do |x|
       (0..7).each do |y|
-        hex = generator.create_hex
-        c   = Color::RGB.by_hex(hex)
+        c = Color::RGB.by_hex(g.gradient(x).to_s(16))
 
         hat[x, y] = Ws2812::Color.new(c.red.to_i, c.green.to_i, c.blue.to_i)
         hat.show
-        sleep 0.05
+        sleep 0.04
       end
     end
   end
