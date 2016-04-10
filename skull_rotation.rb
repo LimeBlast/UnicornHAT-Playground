989f2e016d927648b74b381f08207f5fd520be8f
Daniel Hollands <daniel@foxsoft.co.uk>
2016-04-10 12:35:05 +0100

    Updates skull to use gradient class

--- skull_rotation.rb
+++ skull_rotation.rb
@@ -2,28 +2,29 @@
 require 'ws2812'
 require 'color'
 require 'color-generator'
-require 'chroma'
+require 'color_gradient'
 
 require_relative 'skull'
 
 def display(array)
   $hat.clear false
 
-  hex = ColorGenerator.new(saturation: 0.3, lightness: 0.75).create_hex
+  start = Color::RGB.by_hex($cg.create_hex)
+  stop  = Color::RGB.by_hex($cg.create_hex)
+  g     = ColorGradient.new(start, stop, 7)
 
   array.each_with_index do |row, x|
-    c = Color::RGB.by_hex(hex)
+    c      = g.gradient(x)
     colour = Ws2812::Color.new(c.red.to_i, c.green.to_i, c.blue.to_i)
 
     row.each_with_index do |pixel, y|
       $hat[x, y] = colour if pixel > 0
     end
-
-    hex = hex.paint.darken(6).to_hex
   end
   $hat.show
 end
 
+$cg  = ColorGenerator.new(saturation: 0.75, lightness: 0.4)
 $hat = Ws2812::UnicornHAT.new
 
 $hat.rotation = 90
@@ -36,7 +37,5 @@ begin
     sleep 1.5
   end
 rescue Interrupt
-# ignored
+  $hat.clear
 end
-
-$hat.clear
