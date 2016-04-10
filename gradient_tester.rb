0270c47f3d45e6c2302cdb7eb3d88ad2b879b967
Daniel Hollands <daniel@foxsoft.co.uk>
2016-04-10 12:17:17 +0100

    Added my ColorGradient gem, and modified the code to use it

--- gradient_tester.rb
+++ gradient_tester.rb
@@ -2,8 +2,7 @@
 require 'ws2812'
 require 'color'
 require 'color-generator'
-
-require_relative 'gradient'
+require 'color_gradient'
 
 cg  = ColorGenerator.new(saturation: 0.75, lightness: 0.4)
 hat = Ws2812::UnicornHAT.new
@@ -13,10 +12,12 @@ begin
   hat.rotation = 90
   hat.clear false
   loop do
-    g = Gradient.new(cg.create_rgb, cg.create_rgb, 7)
+    start = Color::RGB.by_hex(cg.create_hex)
+    stop  = Color::RGB.by_hex(cg.create_hex)
+    g     = ColorGradient.new(start, stop, 7)
     (0..7).each do |x|
       (0..7).each do |y|
-        c = Color::RGB.by_hex(g.gradient(x).to_s(16))
+        c = g.gradient(x)
 
         hat[x, y] = Ws2812::Color.new(c.red.to_i, c.green.to_i, c.blue.to_i)
         hat.show
